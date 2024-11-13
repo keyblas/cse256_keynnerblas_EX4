6c055a5080f7ea2bc819256d4058e9f62812b8d1
keyblas <key2138975@maricopa.edu>
2024-11-13 13:50:59 -0700

    test function test_select_random_animal() has been added to check random animal selected

--- GitHubPython/TEST_CIS256_EX4_KB.py
+++ GitHubPython/TEST_CIS256_EX4_KB.py
@@ -7,3 +7,15 @@ import pytest
 
 # From py file we will import function that will be tested
 from CIS256_EX4_KB import select_random_animal, check_vowel_in_word
+
+# Function will test that random animal selected is in list predefined
+def test_select_random_animal():
+    animal = select_random_animal()
+    assert animal in [
+        "ALLIGATOR", "ALPACA", "ANACONDA", "ANT", "ARMADILLO", 
+        "ELEPHANT", "EAGLE", "EEL", "ELK", "IMPALA", "IBIS",
+        "IGUANA", "IBEX", "INDRI", "OTTER", "OCTOPUS", "ORANGUTAN", 
+        "ORCA", "OWL", "OYSTER", "OSTRICH", "ORIOLE", "UTONAGAN", "UNAU", 
+        "URUTU", "UMBRELLABIRD", "URCHIN"
+    ]
+
